1e585a85cd3ea4a254242cdd59b5b19888a76ebd
Nik Jenic <jenic.nik@gmail.com>
2021-10-05 20:49:08 +0200

    Added fractal tree. You can rotate the branches by selecting either 1 or 2 on keyboard, then use scroll wheel to rotate them

--- MyFrame.java
+++ MyFrame.java
@@ -1,13 +1,51 @@
 import javax.swing.*;
-public class MyFrame extends JFrame{
+import java.awt.event.*;
+public class MyFrame extends JFrame implements MouseWheelListener, KeyListener{
     
-    MyFrame(MyPanel panel){
+    MyPanel panel = new MyPanel();
+    MyFrame(){
 
         this.add(panel);
 
-        this.setSize(800,800);
+        this.addMouseWheelListener(this);
+        this.addKeyListener(this);
+
+        this.pack();
         this.setLocationRelativeTo(null);
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         this.setVisible(true);
+    }
+
+    int selectedLine = 0;
+
+    @Override
+    public void mouseWheelMoved(MouseWheelEvent e) {
+        // TODO Auto-generated method stub
+        if(selectedLine==1) MyPanel.angle2+=e.getWheelRotation();
+        else if(selectedLine==2) MyPanel.angle1+=e.getWheelRotation();
+        repaint();
+    }
+
+    @Override
+    public void keyPressed(KeyEvent e) {
+        // TODO Auto-generated method stub
+        System.out.println(e.getKeyCode());
+        if(e.getKeyCode()==49){
+            selectedLine = 1;
+        } else if(e.getKeyCode()==50){
+            selectedLine = 2;
+        }
+    }
+
+    @Override
+    public void keyTyped(KeyEvent e) {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    public void keyReleased(KeyEvent e) {
+        // TODO Auto-generated method stub
+        
     }
 }
--- MyPanel.java
+++ MyPanel.java
@@ -1,11 +1,55 @@
 import java.awt.Color;
+import java.awt.image.BufferStrategy;
 
 import javax.swing.*;
-public class MyPanel extends JPanel{
+import java.awt.*;
+import java.awt.event.*;
+public class MyPanel extends JPanel implements ActionListener{
     
-    MyFrame frame = new MyFrame(this);
+    Graphics2D g;
+    //MyFrame frame = new MyFrame(this);
+    static final int PANEL_WIDTH = 800;
+    static final int PANEL_HEIGHT = 800;
     MyPanel(){
-        this.setSize(frame.getWidth(), frame.getHeight());
-        this.setBackground(Color.black);
+        this.setPreferredSize(new Dimension(PANEL_WIDTH, PANEL_HEIGHT));
+        Timer timer = new Timer(0, this);
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        // TODO Auto-generated method stub
+        repaint();
+    }
+
+    @Override
+    public void paint(Graphics g){
+        g.setColor(Color.black);
+        g.fillRect(0, 0, PANEL_WIDTH, PANEL_HEIGHT);
+        g.setColor(Color.white);
+        this.g = (Graphics2D) g;
+        int segmentLength=this.getHeight();
+        fractal(PANEL_WIDTH/2, PANEL_HEIGHT, segmentLength);
+    }
+
+    static double angle1 = 60;
+    static double angle2 = -60;
+    void fractal(int x, int y, int segmentLength){
+        if(segmentLength*10<1) return;
+        g.translate(x, y);
+        int drawLength = segmentLength/4;
+        g.drawLine(0, 0, 0, -drawLength);
+
+        g.translate(0, -drawLength);
+
+        g.rotate(angle1/180*Math.PI);
+        fractal(0, 0, segmentLength/3*2);
+        //g.translate(0, segmentLength);
+        g.rotate(-angle1/180*Math.PI);
+
+
+        g.rotate((angle2)/180*Math.PI);
+        fractal(0, 0, segmentLength/3*2);
+        g.rotate(-(angle2)/180*Math.PI);
+        g.translate(0, drawLength);
     }
 }
